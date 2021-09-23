0e87e23db032d688a2e8e225b0d4dd569cbe17bd
Guillermo Cabrera <gcabrerojas@gmail.com>
2021-09-23 17:59:10 -0300

    layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import { Component } from "react";
 import Productos from './components/Productos'
+import Layout from './components/Layout'
 
 class App extends Component {
   state = {
     productos: [
-      { name: 'Tomate', price: 1500, img: '/productos/tomate.jpg'},
-      { name: 'Arbejas', price: 2500, img: '/productos/arbejas.jpg'},
-      { name: 'Lechuga', price: 500, img: '/productos/lechuga.jpg'},
+      { name: 'Tomate', price: 1500, img: '/productos/tomate.jpg' },
+      { name: 'Arbejas', price: 2500, img: '/productos/arbejas.jpg' },
+      { name: 'Lechuga', price: 500, img: '/productos/lechuga.jpg' },
     ]
   }
-  render(){
+  render() {
     return (
       <div>
-        <Productos 
-          agregarAlCarro={()=> console.log('hace nada')}
-          productos = {this.state.productos}
-        />
+        <Layout>
+          <Productos
+            agregarAlCarro={() => console.log('hace nada')}
+            productos={this.state.productos}
+          />
+        </Layout>
+
       </div>
     )
   }
